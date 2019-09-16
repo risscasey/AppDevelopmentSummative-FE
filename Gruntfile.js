43cd48e19ffdc5237026c401c1e6dbd6b9b3a3ba
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-09-17 10:08:54 +1200

    Gruntfile edited

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,21 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-
-
+    uglify: {
+      my_target: {
+        files: {
+          'js/server.min.js': ['js/server.js']
+        }
+      }
+    },
+    csslint: {
+      strict: {
+        options: {
+          import: 2
+        },
+        src: ['css/*.css', 'css/!*.min.css']
+      }
+    }
     // Annies tasks untill here
 
 
@@ -14,6 +27,13 @@ module.exports = function(grunt) {
 
   });
 
+  // Load grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+
+  // Register tasks
+  grunt.registerTask('minJs', ['uglify']);
+  grunt.registerTask('checkCss', ['csslint']);
   // Calling Annies tasks untill here
 
 
