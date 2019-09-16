9d97211f60b26416f89275ac908c8cb9637c2804
Larissa Godfrey <932400452@bun2-206-21.acg.ac.nz>
2019-09-17 09:14:51 +1200

    relocated grunt fiile

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+
+    // Annies tasks untill here
+
+
+    // Larissas tasks untill here
+
+
+    // Katherines tasks untill here
+
+  });
+
+  // Calling Annies tasks untill here
+
+
+  // Calling Larissas tasks untill here
+
+
+  // Calling Katherines tasks untill here
+
+};
