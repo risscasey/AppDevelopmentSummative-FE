15bfdc72e2fd3253c3041b9b6281f64f9f2c2201
Larissa Godfrey <932400452@bun2-206-21.acg.ac.nz>
2019-09-17 11:23:58 +1200

    added functional scss compiling and js hinting

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,21 @@ module.exports = function(grunt) {
         },
         src: ['css/*.css', 'css/!*.min.css']
       }
-    }
+    },
     // Annies tasks untill here
-
+    sass: {
+      dist: {
+        files: {
+          'css/style.css': 'scss/style.scss'
+        }
+      }
+    },
+    jshint: {
+      files: ['js/*.js','js/!*.min.js'],
+        options: {
+          esversion: 6
+        }
+    },
 
     // Larissas tasks untill here
 
@@ -37,8 +49,19 @@ module.exports = function(grunt) {
   // Calling Annies tasks untill here
 
 
+  // Load grunt tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Register tasks
+  grunt.registerTask('hintJS', ['jshint']);
+  grunt.registerTask('checkSASS', ['sass']);
   // Calling Larissas tasks untill here
 
+
+  // Load grunt tasks
+
+  // Register tasks
 
   // Calling Katherines tasks untill here
 
