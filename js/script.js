1a7924d6a593244f4e6ab8467ad1477dd73b4b97
Larissa Godfrey <932400452@bun2-204-17.local>
2019-09-17 12:49:37 +1200

    add listing form completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+$('#addListing').click(function() {
+  event.preventDefault();
+  $('#addlistingForm').removeClass('d-none');
+});
+
+$('#subitNewListing').click(function() {
+
+  let itemName = $('#itemName').val();
+  let itemPrice = $('#itemPrice').val();
+  let itemDescription = $('#itemDescription').val();
+  let itemSeller = $('#itemSeller').val();
+
+  let newListing = itemName + ' $' + itemPrice + ' ' + itemDescription + ' ' + itemSeller;
+
+  console.log(newListing);
+});
