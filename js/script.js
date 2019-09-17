210ecfd08bd89b3b8e6440a057d4335df5393231
Annie Isa'ako <270020786@bun2-204-19.local>
2019-09-17 13:24:54 +1200

    models folder

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log('hello world');
+console.log('js has loaded');
+$.ajax({
+    url: './config.json',
+    type: 'GET',
+    dataType: 'json',
+    success:function(keys){
+        url = `${keys.SERVER_URL}:${keys.SERVER_PORT}`;
+    },
+    error: function(){
+        console.log('cannot find config.json file, cannot run application');
+    }
+});
+
+$('#register').click(function() {
+  event.preventDefault();
+  console.log('button clicked');
+
+  let fName = $('#fName').val();
+  let username = $('#username').val();
+  let password = $('#password').val();
+  let email = $('#email').val();
+  $.ajax({
+    url: `${url}/users`,
+    type: 'POST',
+    data: {
+      fullName: fName,
+      username: username,
+      password: password,
+      email: email
+    },
+    success:function(result){
+      console.log(result);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  })
+});
